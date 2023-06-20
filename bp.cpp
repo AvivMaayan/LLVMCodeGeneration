c1852a0c44ab55165ba4ba2a4d503f9c333c7f9f
nitai.kluger <nitai.kluger@campus.technion.ac.il>
2023-06-20 10:30:33 +0300

    documentation changes

--- bp.cpp
+++ bp.cpp
@@ -7,13 +7,18 @@ using namespace std;
 bool replace(string &str, const string &from, const string &to, const BranchLabelIndex index);
 
 CodeBuffer::CodeBuffer() : buffer(), globalDefs(), regCounter(0) {}
-
+/**
+ * With the Singleton design pattern, the single instance of CodeBuffer could be accessed using:
+ * CodeBuffer &buffer = CodeBuffer::instance();
+ */
 CodeBuffer &CodeBuffer::instance()
 {
     static CodeBuffer inst; // Only instance
     return inst;
 }
-
+/**
+ * generates a jump location label for the next command, writes it to the buffer and returns it
+ */
 string CodeBuffer::genLabel()
 {
     std::stringstream label;
@@ -24,20 +29,38 @@ string CodeBuffer::genLabel()
     emit(label.str());
     return ret;
 }
-
+/**
+ * helper function that returns (the name of) a fresh variable
+ */
 string CodeBuffer::genReg(bool isGlobal)
 {
     string regPrefix = (isGlobal) ? "@" : "%";
     return regPrefix + "var_" + std::to_string(regCounter++);
     ;
 }
-
+/**
+ * writes command to the buffer, returns its location in the buffer
+ */
 int CodeBuffer::emit(const string &s)
 {
     buffer.push_back(s);
     return buffer.size() - 1;
 }
-
+/**
+accepts a list of {buffer_location, branch_label_index} items and a label.
+For each {buffer_location, branch_label_index} item in address_list, backpatches the branch command
+at buffer_location, at index branch_label_index (FIRST or SECOND), with the label.
+note - the function expects to find a '@' char in place of the missing label.
+note - for unconditional branches (which contain only a single label) use FIRST as the branch_label_index.
+example #1:
+int loc1 = emit("br label @");  - unconditional branch missing a label. ~ Note the '@' ~
+bpatch(makelist({loc1,FIRST}),"my_label"); - location loc1 in the buffer will now contain the command "br label %my_label"
+note that index FIRST referes to the one and only label in the line.
+example #2:
+int loc2 = emit("br i1 %cond, label @, label @"); - conditional branch missing two labels.
+bpatch(makelist({loc2,SECOND}),"my_false_label"); - location loc2 in the buffer will now contain the command "br i1 %cond, label @, label %my_false_label"
+bpatch(makelist({loc2,FIRST}),"my_true_label"); - location loc2 in the buffer will now contain the command "br i1 %cond, label @my_true_label, label %my_false_label"
+*/
 void CodeBuffer::bpatch(const vector<LabelLocation> &address_list, const std::string &label)
 {
     for (vector<LabelLocation>::const_iterator i = address_list.begin(); i != address_list.end(); i++)
@@ -47,7 +70,9 @@ void CodeBuffer::bpatch(const vector<LabelLocation> &address_list, const std::st
         replace(buffer[address], "@", "%" + label, labelIndex);
     }
 }
-
+/**
+ * prints the content of the code buffer to stdout
+ */
 void CodeBuffer::printCodeBuffer()
 {
     for (std::vector<string>::const_iterator it = buffer.begin(); it != buffer.end(); ++it)
@@ -55,14 +80,19 @@ void CodeBuffer::printCodeBuffer()
         cout << *it << endl;
     }
 }
-
+/**
+ * gets a pair<int,BranchLabelIndex> item of the form
+ * {buffer_location, branch_label_index} and creates a list for it
+ */
 vector<LabelLocation> CodeBuffer::makelist(LabelLocation item)
 {
     vector<LabelLocation> newList;
     newList.push_back(item);
     return newList;
 }
-
+/**
+ * merges two lists of {buffer_location, branch_label_index} items
+ */
 vector<LabelLocation> CodeBuffer::merge(const vector<LabelLocation> &l1, const vector<LabelLocation> &l2)
 {
     vector<LabelLocation> newList(l1.begin(), l1.end());
@@ -71,11 +101,16 @@ vector<LabelLocation> CodeBuffer::merge(const vector<LabelLocation> &l1, const v
 }
 
 // ******** Methods to handle the global section ********** //
+/**
+ * write a line to the global section
+ */
 void CodeBuffer::emitGlobal(const std::string &dataLine)
 {
     globalDefs.push_back(dataLine);
 }
-
+/**
+ * print the content of the global buffer to stdout
+ */
 void CodeBuffer::printGlobalBuffer()
 {
     for (vector<string>::const_iterator it = globalDefs.begin(); it != globalDefs.end(); ++it)
@@ -101,7 +136,9 @@ bool replace(string &str, const string &from, const string &to, const BranchLabe
     str.replace(pos, from.length(), to);
     return true;
 }
-
+/**
+ * To test our code generation
+ */
 void CodeBuffer::testBuffer()
 {
     cout << "\n\n**** Buffer Testing ****\n\n";
@@ -113,6 +150,16 @@ void CodeBuffer::testBuffer()
     printCodeBuffer();
 }
 
+/** Methods for creating and getting addresses of varibales in the stack*/
+
+/**
+ * Alloc a new register to possess the vakue in the stack of the variable
+ * located in the specific offset after the rbp address of the scope.
+ * @param rbp the base address of the scope
+ * @param offset the offset (positive) from this base address
+ *
+ * @return the name of the register with the varibale value inside of it
+ */
 string CodeBuffer::loadVaribale(string rbp, int offset)
 {
     string reg = genReg();
@@ -123,7 +170,13 @@ string CodeBuffer::loadVaribale(string rbp, int offset)
     emit(reg + " = load i32, i32* " + varPtr);
     return reg;
 }
-
+/**
+ * Store a new variable in the offset address on the stack after rbp. The value
+ * is identified with "reg" register name.
+ * @param rbp the base address of the scope
+ * @param offset the offset (positive) from this base address
+ * @param reg the name of the register holding the value to store
+ */
 void CodeBuffer::storeVariable(string rbp, int offset, string reg)
 {
     string varPtr = genReg();
@@ -132,7 +185,11 @@ void CodeBuffer::storeVariable(string rbp, int offset, string reg)
     /* store in the memory*/
     emit("store i32 " + reg + ", i32* " + varPtr);
 }
-
+/**
+ * Alloc a new register and emit a line stating this register holds the
+ * address of the new base pointer to use (the rbp)
+ * @return the name of the register holding the rbp value
+ */
 string CodeBuffer::allocFunctionRbp()
 {
     string rbp = genReg();
@@ -141,3 +198,6 @@ string CodeBuffer::allocFunctionRbp()
     emit(rbp + " = alloca i32, i32 50");
     return rbp;
 }
+
+/**************** Emit specific code methods *******************/
+
--- bp.hpp
+++ bp.hpp
@@ -23,85 +23,44 @@ class CodeBuffer
     void operator=(CodeBuffer const &);
     std::vector<std::string> buffer;
     std::vector<std::string> globalDefs;
-
+    
     int regCounter;
 
+    /**************** Emit specific code methods *******************/
+    
+
 public:
-    // With the Singleton design pattern, the single instance of CodeBuffer could be accessed using:
-    // CodeBuffer &buffer = CodeBuffer::instance();
     static CodeBuffer &instance();
 
     // ******** Methods to handle the code section ******** //
 
-    // generates a jump location label for the next command, writes it to the buffer and returns it
     std::string genLabel();
 
-    // helper function that returns (the name of) a fresh variable
     std::string genReg(bool isGlobal = false);
 
-    // writes command to the buffer, returns its location in the buffer
     int emit(const std::string &command);
 
-    // gets a pair<int,BranchLabelIndex> item of the form {buffer_location, branch_label_index} and creates a list for it
     static vector<LabelLocation> makelist(LabelLocation item);
 
-    // merges two lists of {buffer_location, branch_label_index} items
     static vector<LabelLocation> merge(const vector<LabelLocation> &l1, const vector<LabelLocation> &l2);
 
-    /* accepts a list of {buffer_location, branch_label_index} items and a label.
-    For each {buffer_location, branch_label_index} item in address_list, backpatches the branch command
-    at buffer_location, at index branch_label_index (FIRST or SECOND), with the label.
-    note - the function expects to find a '@' char in place of the missing label.
-    note - for unconditional branches (which contain only a single label) use FIRST as the branch_label_index.
-    example #1:
-    int loc1 = emit("br label @");  - unconditional branch missing a label. ~ Note the '@' ~
-    bpatch(makelist({loc1,FIRST}),"my_label"); - location loc1 in the buffer will now contain the command "br label %my_label"
-    note that index FIRST referes to the one and only label in the line.
-    example #2:
-    int loc2 = emit("br i1 %cond, label @, label @"); - conditional branch missing two labels.
-    bpatch(makelist({loc2,SECOND}),"my_false_label"); - location loc2 in the buffer will now contain the command "br i1 %cond, label @, label %my_false_label"
-    bpatch(makelist({loc2,FIRST}),"my_true_label"); - location loc2 in the buffer will now contain the command "br i1 %cond, label @my_true_label, label %my_false_label"
-    */
     void bpatch(const vector<LabelLocation> &address_list, const std::string &label);
 
-    // prints the content of the code buffer to stdout
     void printCodeBuffer();
 
     // ******** Methods to handle the data section ******** //
-    // write a line to the global section
     void emitGlobal(const string &dataLine);
-    // print the content of the global buffer to stdout
+    
     void printGlobalBuffer();
 
-    // To test our code generation
     void testBuffer();
 
     /** Methods for creating and getting addresses of varibales in the stack*/
-    
-    /**
-     * Alloc a new register to possess the address on the stack of the variable
-     * located in the specific offset after the rbp address of the scope. 
-     * @param rbp the base address of the scope
-     * @param offset the offset (positive) from this base address
-     *
-     * @return the name of the register with the address inside of it 
-    */
+
     string loadVaribale(string rbp, int offset);
 
-    /**
-     * Store a new variable in the offset address on the stack after rbp, identified
-     * with "reg" name. 
-     * @param rbp the base address of the scope  
-     * @param offset the offset (positive) from this base address
-     * @param reg the name of the register to use for storing the value
-    */
     void storeVariable(string rbp, int offset, string reg);
     
-    /**
-     * Alloc a new register and emit a line stating this register holds the
-     * address of the new base pointer to use (the rbp)
-     * @return the name of the register holding the rbp value
-    */
     string allocFunctionRbp();
 };
 
